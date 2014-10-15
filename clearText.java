ec58b5d4fca7c679e40b220ea5d9a6105547467b
TrolloByte <Dam1403@rit.edu>
2014-10-15 17:57:21 -0400

    Create clearText.java
    Just the unfinished function loader and the Skeleton Command Parser

---
+++ clearText.java
@@ -0,0 +1,40 @@
+import fileName;
+#REFACTOR FUNCTION CLASSS!!!!!!
+
+public class clearText{
+  HashMap<String,functObj> functObjs;
+  
+  
+  public static main(String[] args){
+    #LOADFUNCTIONS
+    
+    Runtime = rt = Runtime.getRuntime();
+    Process pr = rt.exec("javac FUNCTFileNAMEHERE")
+    rt = null;
+    pr = null;
+    
+    functObjs = fileName()
+  
+   line = "";
+   Scanner in = new Scanner(System.in);
+   while(line != "EXIT"){
+    line = in.next();
+    System.out.println(line);
+    
+    
+    
+   }
+    
+    
+    #COMPILE FILE
+  
+  
+  
+    }
+
+
+
+
+
+
+}
