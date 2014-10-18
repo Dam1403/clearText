09918554051033c7caaedc5ae8575c81f3c960d2
TrolloByte <Dam1403@rit.edu>
2014-10-18 14:42:21 -0400

    Create FunctObj.java

---
+++ FunctObj.java
@@ -0,0 +1,14 @@
+public interface FunctObj{
+	
+
+	private String Name;
+
+	public FunctObj();
+
+	public String transmute(String text, String alphabet, int Nonce);
+
+	public String recover(String cText, String alphabet, int Nonce);
+
+	public String getName();
+	
+}
