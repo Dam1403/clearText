1d6469a6fd0a03561c8dea90b3573af7dfdc34ea
TrolloByte <Dam1403@rit.edu>
2014-10-25 20:26:36 -0400

    Create classLoader.java

---
+++ functObjs/classLoader.java
@@ -0,0 +1,25 @@
+public class classLoader{
+	
+	functTable functions;
+	
+  
+  public classLoader(){
+      this.functions = null;
+  }
+	public static void load(){
+		
+		FunctObj[] functObjs = ["@Load"];
+		for(int i = 0; i < functObjs.length(); i++){
+			functions.put(functObj.getName(),functObj);
+		}
+
+	}
+
+	public void setTable(functTable functions){
+		this.functions = functions;
+
+	}
+	
+
+
+}
