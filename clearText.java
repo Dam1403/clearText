9cf423d5f548888a020dbda05d450683bd76d0bd
TrolloByte <Dam1403@rit.edu>
2014-10-29 03:39:46 -0400

    Update clearText.java

--- clearText.java
+++ clearText.java
@@ -1,40 +1,26 @@
-import fileName;
-#REFACTOR FUNCTION CLASSS!!!!!!
+import java.util.Scanner;
+import java.util.HashMap;
+
+import functObjs.FunctObj;
+
+
+/*#REFACTOR FUNCTION CLASSS!!!!!!*/
 
 public class clearText{
-  HashMap<String,functObj> functObjs;
-  
-  
-  public static main(String[] args){
-    #LOADFUNCTIONS
-    
-    Runtime = rt = Runtime.getRuntime();
-    Process pr = rt.exec("javac FUNCTFileNAMEHERE")
-    rt = null;
-    pr = null;
-    
-    functObjs = fileName()
-  
-   line = "";
-   Scanner in = new Scanner(System.in);
-   while(line != "EXIT"){
-    line = in.next();
-    System.out.println(line);
-    
-    
-    
-   }
-    
-    
-    #COMPILE FILE
-  
-  
-  
-    }
+	HashMap<String,FunctObj> functObjs;
 
 
+	public static void main(String[] args){
 
+		
+		functTable functObjs = refInit.init(clearText.class.getClassLoader());	
+		String line = "";
+		Scanner in = new Scanner(System.in);
+		while(line != "EXIT"){
+			line = in.next();
+			System.out.println(line);
 
-
+		}
+	}
 
 }
