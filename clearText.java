511b61ecebe514c3977a38b1e105486abdd328d2
Douglas May <Dam1403@rit.edu>
2014-11-10 11:44:51 -0500

    Bug FIxing
    Bugs destroyed
    -Allows text to be set with quotes
    -Allows the user to specify a quote character to prevent collisions
    while setting text
    -Made the display for some functions look better
    -Looking for a better way to handle sets
    - Added setquote to help function

--- clearText.java
+++ clearText.java
@@ -6,15 +6,16 @@ import java.io.FileReader;
 import java.io.FileNotFoundException;
 
 import functObjs.FunctObj;
-
+//YOU WERE TESTING QUOTES
 
 /*#REFACTOR FUNCTION CLASSS!!!!!!*/
 
 public class clearText{
 
 	private static HashMap<String,FunctObj> functObjs;
-	private static String defAlphabet = "abcdefghijklmnopqrstuvwxyz";
 	private static String userAlphabet = null;
+
+	private static String userQuote = "\"";
 	private static ArrayList<String> history = new ArrayList<String>();
 
 	private static String text = null;
@@ -35,7 +36,7 @@ public class clearText{
 			String input = null;
 	
 
-			if(arguments[0].equals("settxt")){
+			if(arguments[0].equals("settext")){
 				if(arguments.length == 2){
 					text = set(arguments[1],false);
 				}
@@ -46,7 +47,7 @@ public class clearText{
 				else{
 					text = set(arguments[1],arguments[2].equals("-f"));
 				}
-				System.out.println("Text set to :" + text);
+				System.out.println("Text set to " + text);
 			}
 
 
@@ -62,7 +63,22 @@ public class clearText{
 				else{
 					userAlphabet = set(arguments[1],arguments[2].equals("-f"));
 				}
-				System.out.println("Alphabet set to :" + userAlphabet);
+				System.out.println("Alphabet set to " + userAlphabet);
+			}
+			//FIND BETTER WAY TO HANDLE THESE SETS
+			else if(arguments[0].equals("setquote")){
+				
+				if(arguments.length == 2){
+					userQuote = set(arguments[1],false);
+				}
+				else if(arguments.length == 1 || arguments.length > 3 ){
+					System.out.println("Invalid argument Length");
+					continue;
+				}
+				else{
+					userQuote = set(arguments[1],arguments[2].equals("-f"));
+				}
+				System.out.println("quote set to " + userQuote);
 			}
 			else if(arguments[0].equals("history")){
 				if(arguments.length == 1){
@@ -86,6 +102,11 @@ public class clearText{
 				System.out.println("Command: setalpha");
 				System.out.println("\tusage: setalpha [String your alphabet] ");
 				System.out.println("\tusage: setalpha [alphabet filename]  -fn\n");
+				System.out.println("Command: setquote");
+				System.out.println("\tusage: setquote [String your quote] ");
+				System.out.println("\tusage: setquote [quote filename]  -fn\n");
+				System.out.println("\tDefaults to \" set to a palindrome\n");
+
 				System.out.println("Command: history");
 				System.out.println("\tusage: history");
 				System.out.println("\tusage: history [Number to display]\n");
@@ -105,11 +126,11 @@ public class clearText{
 					continue;
 				}
 				System.out.println(fObj.getName());
-				text = fObj.transmute(text,defAlphabet,0);
+				text = fObj.transmute(text,text,0);
 				System.out.println(text);
 				for(int i = 1; i<arguments.length;i++){
 					fObj = functObjs.get(arguments[i]);
-					text = fObj.transmute(text,defAlphabet,0);
+					text = fObj.transmute(text,userAlphabet,0);
 					System.out.println(fObj.getName());
 					System.out.println(text);
 				}
@@ -177,19 +198,38 @@ public class clearText{
 		ArrayList<String> arguments = new ArrayList<String>();
 		String currString = "";
 		char c = 0;
+
+		boolean quote = false;
+
 		for(int i = 0; i < line.length();i++){
 			c = line.charAt(i);
+
 			if(c == ' ' || i == line.length()){
+				if(quote){
+					
+					currString += c;
+					continue;
+				}
 				arguments.add(currString);
 				currString = "";		
 			}
+			//MAKE USER SET!!!
+			//May be able to be done with subString
+			else if (c == userQuote.charAt(0)){
+				int potInd = i + userQuote.length();
+				if(userQuote.equals(line.substring(i,potInd))){
+					quote = !quote;
+					i += userQuote.length();
+				}
+			}
+
 			else{
 				currString += c;
 
 			}
 		}
+		arguments.add(currString);
 		String[] array = new String[arguments.size()];
-		System.out.println(arguments.size());
 		array = arguments.toArray(array);
 		return array;
 	}
