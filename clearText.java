03b38e349916d0cd2e6fcbba4a042cd0b65c0020
Douglas May <Dam1403@rit.edu>
2014-11-28 23:03:19 -0500

    Javadocs view
    view current text and current alphabet

--- clearText.java
+++ clearText.java
@@ -13,7 +13,7 @@ import functObjs.FunctObj;
 public class clearText{
 
 	private static HashMap<String,FunctObj> functObjs;
-	private static String userAlphabet = null;
+	private static String userAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
 	private static String userQuote = "\"";
 	private static ArrayList<String> history = new ArrayList<String>();
@@ -32,7 +32,7 @@ public class clearText{
 	 * 	corectly such as the simple caesar cipher 
 	 * 	alphabet Example| a CaesRight over the normal english alphabet 
 	 * 		performed on "cat" would return "dbu"
-	 * userQuote - If your copying and pasting Text into the shell 
+	 * userQuote - If your copying and pasting Text into the shell
 	 * 	using syntax settext "[blog paragraph]" any double quotes in the
 	 * 	text would cut the argument short to solve this I allow the user
 	 * 	to set their own custom palindrome quote string 
@@ -43,7 +43,7 @@ public class clearText{
 	 *
 	 * text - The Text the user wants encryted or decrypted
 	 *
-	 *  @param args currently useless
+	 * @param args currently useless
 	 */
 	public static void main(String[] args){
 		
@@ -59,12 +59,23 @@ public class clearText{
 		while(!line.equals("EXIT")){
 			System.out.print(">");
 			line = in.nextLine().trim();
+
 			if(!line.equals("history")){history.add(line);}
+
 			String[] arguments = lineHandler(line);
 			FunctObj fObj = functObjs.get(arguments[0]);
 			String input = null;
-	
 
+			if(arguments[0].equals("text")){
+				System.out.println("Text is currently:\n");
+				System.out.println(text + "\n");
+				continue;
+			}	
+			if(arguments[0].equals("alphabet")){
+				System.out.println("Alphabet is currently:\n");
+				System.out.println(userAlphabet + "\n");
+				continue;
+			}	
 			if(arguments[0].equals("settext")){
 				if(arguments.length == 2){
 					text = set(arguments[1],false);
@@ -77,6 +88,7 @@ public class clearText{
 					text = set(arguments[1],arguments[2].equals("-f"));
 				}
 				System.out.println("Text set to " + text);
+				continue;
 			}
 
 
@@ -93,6 +105,7 @@ public class clearText{
 					userAlphabet = set(arguments[1],arguments[2].equals("-f"));
 				}
 				System.out.println("Alphabet set to " + userAlphabet);
+				continue;
 			}
 			//FIND BETTER WAY TO HANDLE THESE SETS
 			else if(arguments[0].equals("setquote")){
@@ -108,6 +121,7 @@ public class clearText{
 					userQuote = set(arguments[1],arguments[2].equals("-f"));
 				}
 				System.out.println("quote set to " + userQuote);
+				continue;
 			}
 			else if(arguments[0].equals("history")){
 				if(arguments.length == 1){
@@ -115,6 +129,7 @@ public class clearText{
 					continue;
 				}	
 				history(Integer.parseInt(arguments[1]));
+				continue;
 
 			}
 			
@@ -123,6 +138,12 @@ public class clearText{
 				if(arguments.length == 2){
 					dispTable(functObjs,arguments[1]);
 				}
+
+				System.out.println("Command: text");
+				System.out.println("\tusage: text");
+
+				System.out.println("Command: alphabet");
+				System.out.println("\tusage: alphabet");
 
 				System.out.println("Command: settxt");
 				System.out.println("\tusage: settxt [your text] ");
@@ -134,7 +155,7 @@ public class clearText{
 				System.out.println("Command: setquote");
 				System.out.println("\tusage: setquote [String your quote] ");
 				System.out.println("\tusage: setquote [quote filename]  -fn\n");
-				System.out.println("\tDefaults to \" set to a palindrome\n");
+				System.out.println("\tDefaults to \". set to a palindrome\n");
 
 				System.out.println("Command: history");
 				System.out.println("\tusage: history");
@@ -143,10 +164,7 @@ public class clearText{
 				System.out.println("\tusage: help");
 				System.out.println("\tusage: help [FunctObjName]\n");
 				dispTable(functObjs, null);
-
-
-
-
+				continue;
 
 			}
 			else if(fObj != null){
@@ -155,7 +173,7 @@ public class clearText{
 					continue;
 				}
 				System.out.println(fObj.getName());
-				text = fObj.transmute(text,text,0);
+				text = fObj.transmute(text,userAlphabet,0);
 				System.out.println(text);
 				for(int i = 1; i<arguments.length;i++){
 					fObj = functObjs.get(arguments[i]);
@@ -163,12 +181,22 @@ public class clearText{
 					System.out.println(fObj.getName());
 					System.out.println(text);
 				}
+				continue;
 			}
+			history.remove(history.size() - 1);
 			
 
 		}
 	}
-
+	/**
+	 * This function is used to set the text, and alphabet used 
+	 * during the transmution process
+	 * It reads the text from a file or from the commandline
+	 *
+	 * @param theText Either a filename or command line text 
+	 * @param isFilename true or false if the user wants the text from a file
+	 *
+	 */
 	private static String set(String theText, boolean isFilename){
 		String newText = "";
 		if(isFilename){
@@ -185,7 +213,12 @@ public class clearText{
 		return theText;
 
 	}
-
+	/**
+	 * prints the last arguments entered into the command line
+	 *
+	 * @param histNum controls how many entries to print from the history 
+	 *
+	 */
 	private static void history(int histNum){
 		if(histNum == 0){
 			histNum = history.size();
@@ -196,7 +229,13 @@ public class clearText{
 	}
 
 
-
+	/**
+	 * Returns the help function for a specific Object
+	 * 
+	 * @param table The table containing the functions
+	 * @param objName The name of the object 
+	 *
+	 */
 	private static void dispTable(functTable table, String objName){
 		if(objName != null){
 
@@ -222,7 +261,13 @@ public class clearText{
 			System.out.println(vals.get(i).help());
 		}
 	}
-
+	
+	/** Handles the line recieved from the user
+	 * It splits the line by space and groups strings together based on
+	 * the user specified quote pattern userQuote
+	 *
+	 * @param line The line from standard input
+	 */
 	private static String[] lineHandler(String line){
 		ArrayList<String> arguments = new ArrayList<String>();
 		String currString = "";
@@ -248,7 +293,8 @@ public class clearText{
 				int potInd = i + userQuote.length();
 				if(userQuote.equals(line.substring(i,potInd))){
 					quote = !quote;
-					i += userQuote.length();
+					i += userQuote.length() - 1;
+					
 				}
 			}
 
